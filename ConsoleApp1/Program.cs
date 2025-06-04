0dd706bc20204fcbd7e895713c4aa8be7a0b7ac5
Minh Sáng <minhsang.1mil@gmail.com>
2025-06-04 10:59:54 +0700

    remove unnecessary files

--- ConsoleApp1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using ConsoleApp1;
-using Specification.Evaluators;
-using Specification.Interfaces;
-
-ISpecification<User> a = new UserCombineSpec();
-IQueryable<User> user = Enumerable.Empty<User>().AsQueryable();
-var b = SpecificationEvaluator.GetQuery<User>(user, a);
